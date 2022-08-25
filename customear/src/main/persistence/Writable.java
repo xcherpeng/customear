9869080a661958e339bf5ea230820dc0b20ad18a
xcherpeng <70073029+xcherpeng@users.noreply.github.com>
2022-08-25 13:56:07 -0700

    Delete customear directory

--- customear/src/main/persistence/Writable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-// based on CPSC 210 Workspace project (JSON serialization demo)
-public interface Writable {
-    // EFFECTS: returns this as JSON object
-    JSONObject toJson();
-}
